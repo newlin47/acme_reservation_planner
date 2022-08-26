542d255e923bab0b28da5eef5278fedf33e3de2f
Ethan Newlin <newlin5@gmail.com>
2022-08-25 20:14:42 -0400

    showing users and restaurants

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log('hello world');
+const usersList = document.querySelector('#users-list');
+const restaurantsList = document.querySelector('#restaurants-list');
+
+const setup = async () => {
+	console.log('starting');
+	const responseUsers = await fetch('/api/users');
+	const users = await responseUsers.json();
+	const htmlUsers = users
+		.map((user) => {
+			return `<li>${user.name}</li>`;
+		})
+		.join('');
+	usersList.innerHTML = htmlUsers;
+
+	const responseRestaurants = await fetch('/api/restaurants');
+	const restaurants = await responseRestaurants.json();
+	const htmlRestaurants = restaurants
+		.map((restaurant) => {
+			return `<li>${restaurant.name}</li>`;
+		})
+		.join('');
+	restaurantsList.innerHTML = htmlRestaurants;
+};
+setup();
